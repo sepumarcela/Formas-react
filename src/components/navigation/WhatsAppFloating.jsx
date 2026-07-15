99366db9c41105357e0da08db4ecb8dc3b3c5200
sepumarcela <yulimarce0616@gmail.com>
2026-07-15 12:03:35 -0500

    Revert "Aislar overlays de la carga inicial"
    This reverts commit 3f43d96711f43125611bac598abf9fe077cf6180.

--- src/components/navigation/WhatsAppFloating.jsx
+++ src/components/navigation/WhatsAppFloating.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useSiteContent } from '../../hooks/useSiteContent'
 
@@ -16,9 +16,13 @@ function WhatsAppIcon() {
   )
 }
 
-function WhatsAppFloatingContent() {
+function WhatsAppFloating() {
   const [{ pageContent }] = useSiteContent()
+  const { pathname } = useLocation()
   const [visible, setVisible] = useState(true)
+
+  if (pathname.startsWith('/cuenta')) return null
+
   const whatsappLink = pageContent.homeProducts?.finalWhatsappLink || pageContent.contacto?.whatsappLink || 'https://wa.me/573169733417'
 
   return (
@@ -35,25 +39,6 @@ function WhatsAppFloatingContent() {
       </a>
     </div>
   )
-}
-
-function WhatsAppFloating() {
-  const { pathname } = useLocation()
-  const [ready, setReady] = useState(false)
-
-  useEffect(() => {
-    setReady(false)
-    if (pathname.startsWith('/cuenta')) return undefined
-
-    const isMobile = window.matchMedia?.('(max-width: 767px)').matches
-    const timer = window.setTimeout(() => setReady(true), isMobile ? 10000 : 5000)
-
-    return () => window.clearTimeout(timer)
-  }, [pathname])
-
-  if (pathname.startsWith('/cuenta') || !ready) return null
-
-  return <WhatsAppFloatingContent />
 }
 
 export default WhatsAppFloating
